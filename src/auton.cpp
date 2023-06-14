787ad77281a6609b2ba8325c65b8dc253f6fc0df
Storm <120764143+ItzSt0rmz@users.noreply.github.com>
2023-06-14 11:03:03 -0500

    Still fixing bugs

--- src/auton.cpp
+++ src/auton.cpp
@@ -1,6 +1,4 @@
 #include "main.h"
-#include "IncludeFiles.h"
-
 
 const int DRIVE_SPEED = 110; // This is 110/127 (around 87% of max speed).  We don't suggest making this 127.
                              // If this is 127 and the robot tries to heading correct, it's only correcting by

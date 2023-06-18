738662b3f26ecf023555a6b1e2295c35c57080c0
ItzSt0rmz <thecolehawkins@gmail.com>
2023-06-18 11:40:09 -0500

    Added description for ControllerScreen

---
+++ src/AdditionalClasses/ControllerScreen.cpp
@@ -0,0 +1,16 @@
+/**************************************************************
+                CONTROLLER SCREEN
+
+The controller screen data is stored here and will be run as
+a task in main. The controller includes data such as
+ - Battery life
+ - Drive speed
+ - Robot odometry position
+ - Flywheel speed
+etc....
+
+Created 6/17/23
+Last update 6/18/23
+
+****************************************************************/
+#include "main.h"

df3db29ae5768676ee5212a4f06e9a2e4f71090d
ItzSt0rmz <thecolehawkins@gmail.com>
2023-06-17 20:32:25 -0500

    Worked on odometry, made new file system to be more organized

---
+++ src/Outside  Classes/RobotSetup.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+/**************************************************************
+ROBOT SETUP 
+
+Sets up the devices used throughout the program, this includes:
+ - Motors
+ - Controllers
+ - Pneumatics
+ - Sensors
+
+****************************************************************/
+
+// ------------- ALLIANCE ----------- //
+string alliance = {"blue"}; // Blue by default
+
+//

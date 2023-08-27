238758179e52ecec37a97f36f5f6f2f6f6dcdeed
ItzSt0rmz <thecolehawkins@gmail.com>
2023-08-26 20:40:34 -0500

    Updated chassis files to drivetrain

--- include/main.h
+++ include/main.h
@@ -47,7 +47,7 @@
 // More includes here...
 #include "additionalfunctions.hpp"
 #include "robotsetup.hpp"
-#include "chassis.hpp"
+#include "drivetrain.hpp"
 
 /**
  * If you find doing pros::Motor() to be tedious and you'd prefer just to do
